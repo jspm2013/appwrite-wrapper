d32cede14589ad2024e0072a12fe6cc6a92cf814
Sebastian <100091292+jspm2013@users.noreply.github.com>
2025-01-21 14:13:51 +0100

    removed dotenv

--- dist/schemas/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Attribute, Index, CreateRelationshipAttributeParams } from "./types";
-export { createAttribute } from "./createAttribute";
-export { getSchema } from "./getSchema";
-//# sourceMappingURL=index.d.ts.map
--- dist/schemas/index.js
+++
@@ -1,2 +0,0 @@
-export { createAttribute } from "./createAttribute";
-export { getSchema } from "./getSchema";

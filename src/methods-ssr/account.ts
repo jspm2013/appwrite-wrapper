311f3185316acd4b3249c21473d55bc085a63b26
Sebastian <100091292+jspm2013@users.noreply.github.com>
2025-02-18 05:32:31 +0100

    test

--- src/methods-ssr/account.ts
+++ src/methods-ssr/account.ts
@@ -449,7 +449,6 @@ export type CreateOAuth2TokenParams = {
   }
 }; */
 const createOAuth2Token = async (
-  _prevState: ReturnObject<string>,
   params: CreateOAuth2TokenParams
 ): Promise<ReturnObject<string>> => {
   try {

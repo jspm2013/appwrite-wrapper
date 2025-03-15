0b7c12102f3e67ffe0781a3b53bd2a1fad54b537
Sebastian <100091292+jspm2013@users.noreply.github.com>
2025-03-15 08:44:12 +0100

    test

--- dist/methods/users.js
+++ dist/methods/users.js
@@ -227,9 +227,11 @@ const getUserForUserId = async ({ userId, queries = [], includingDeleted = undef
         const { users } = await createAdminClient();
         const { databases } = await createAdminClient();
         const user = await users.get(userId);
+        console.log("getUserForUserId - user1:", user);
         if (!user) {
             throw new Error("No session user found in database.");
         }
+        console.log("getUserForUserId - user2:", user);
         const { total, documents } = await databases.listDocuments(databaseId, userCollectionId, [
             includingDeleted === undefined
                 ? queries.length
@@ -241,6 +243,18 @@ const getUserForUserId = async ({ userId, queries = [], includingDeleted = undef
                     Query.equal("deleted", includingDeleted),
                 ]),
         ]);
+        console.log("Queries: ", [
+            includingDeleted === undefined
+                ? queries.length
+                    ? Query.and([...queries, Query.equal("user_id", userId)])
+                    : Query.equal("user_id", userId)
+                : Query.and([
+                    ...queries,
+                    Query.equal("user_id", userId),
+                    Query.equal("deleted", includingDeleted),
+                ]),
+        ]);
+        console.log("getUserForUserId - total/documents:", total, documents);
         return {
             data: {
                 ...user,
--- src/methods/users.ts
+++ src/methods/users.ts
@@ -366,10 +366,12 @@ const getUserForUserId = async ({
     const { databases } = await createAdminClient();
 
     const user = await users.get(userId);
+    console.log("getUserForUserId - user1:", user);
     if (!user) {
       throw new Error("No session user found in database.");
     }
 
+    console.log("getUserForUserId - user2:", user);
     const { total, documents } = await databases.listDocuments(
       databaseId,
       userCollectionId,
@@ -385,6 +387,18 @@ const getUserForUserId = async ({
             ]),
       ]
     );
+    console.log("Queries: ", [
+      includingDeleted === undefined
+        ? queries.length
+          ? Query.and([...queries, Query.equal("user_id", userId)])
+          : Query.equal("user_id", userId)
+        : Query.and([
+            ...queries,
+            Query.equal("user_id", userId),
+            Query.equal("deleted", includingDeleted),
+          ]),
+    ]);
+    console.log("getUserForUserId - total/documents:", total, documents);
 
     return {
       data: {
